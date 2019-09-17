b7f39f02510989a80245d0f4994972b297eee8f6
mchlvl <mic.vala@gmail.com>
2019-09-17 12:09:19 +0200

    exported get_ip

--- starlette_zipkin/__init__.py
+++ starlette_zipkin/__init__.py
@@ -2,7 +2,9 @@ from starlette_zipkin.middleware import ZipkinConfig
 from starlette_zipkin.middleware import ZipkinMiddleware
 from starlette_zipkin.middleware import get_root_span
 from starlette_zipkin.middleware import get_tracer
+from starlette_zipkin.middleware import get_ip
 from .header_formatters import B3Headers, UberHeaders
+
 
 __version__ = "2019.09.03"
 __all__ = [
@@ -12,4 +14,5 @@ __all__ = [
     "UberHeaders",
     "get_tracer",
     "get_root_span",
+    "get_ip",
 ]
