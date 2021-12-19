ff398682e5effa1221096265dd0534cb73212ce1
Michal Vala <mic.vala@gmail.com>
2021-12-19 17:57:47 +0100

    v0.2.0 (#22)

--- starlette_zipkin/__init__.py
+++ starlette_zipkin/__init__.py
@@ -2,7 +2,7 @@ from starlette_zipkin.header_formatters import B3Headers, UberHeaders
 from starlette_zipkin.middleware import ZipkinConfig, ZipkinMiddleware, get_ip
 from starlette_zipkin.trace import get_root_span, get_tracer, trace
 
-__version__ = "0.1.4"
+__version__ = "0.2.0"
 __all__ = [
     "ZipkinConfig",
     "ZipkinMiddleware",
