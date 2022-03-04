fb25bbe4a91b28bd14828147c2ed8588dc22f3c4
Michal Vala <mic.vala@gmail.com>
2022-03-04 20:27:10 +0100

    Move old examples to a separate folder, removed redundant readme content
    (cherry picked from commit ea873c6b3def1ce9ffd09ecc2ea575aeda315ffc)

--- examples/starlette_old/app.py
+++ examples/starlette_old/app.py
@@ -50,4 +50,4 @@ config = ZipkinConfig(
 app.add_middleware(ZipkinMiddleware, config=config)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info", reload=True)
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, log_level="info", reload=True, workers=1)
