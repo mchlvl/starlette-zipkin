e397f719f22afd8fcaa4fe7168193dd0e0c925d8
Guillaume Gauvrit <guillaume@gauvr.it>
2022-03-05 14:39:16 +0100

    Fix bug in case the parent trace is incomplete, missing headers

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -54,3 +54,35 @@ async def test_dispatch_trace(app, dummy_request, next_response):
         "x-b3-spanid": resp.headers["x-b3-spanid"],
         "x-b3-traceid": resp.headers["x-b3-traceid"],
     }
+
+
+@pytest.mark.asyncio
+async def test_dispatch_trace_buggy_headers(app, dummy_request, next_response):
+    trace_id = "6223635aa7bfb6597d72ac7c4680bfed"
+    config = ZipkinConfig("zipkin.host")
+    middleware = ZipkinMiddleware(app, config=config)
+    # the tracer is initialized on the first dispatch
+    assert middleware.tracer is None
+    resp = await middleware.dispatch(
+        dummy_request(
+            headers={
+                "x-b3-traceid": trace_id,
+                # x-b3-spanid should be here
+            }
+        ),
+        next_response,
+    )
+    assert middleware.tracer is not None
+    assert middleware.tracer._transport is not None
+    assert (
+        str(middleware.tracer._transport._address)
+        == "http://zipkin.host:9411/api/v2/spans"
+    )
+    assert dict(resp.headers) == {
+        "x-b3-flags": "0",
+        "x-b3-sampled": "1",
+        "x-b3-spanid": resp.headers["x-b3-spanid"],
+        "x-b3-traceid": resp.headers["x-b3-traceid"],
+    }
+    # we cannot reuse the traceid if the span id was missing
+    assert trace_id != resp.headers["x-b3-spanid"]
