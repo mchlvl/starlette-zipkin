63850c911fc7d1460c36502bb4d93d132925eccf
Michal Vala <mic.vala@gmail.com>
2022-09-08 19:52:24 +0200

    Make tests explicit (new starlette always returns content-length header)

--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -26,10 +26,8 @@ async def test_dispatch_trace_new_child(app, dummy_request, next_response):
         str(middleware.tracer._transport._address)
         == "http://zipkin.host:9411/api/v2/spans"
     )
-    assert dict(resp.headers) == {
-        "x-b3-spanid": span_id,
-        "x-b3-traceid": trace_id,
-    }
+    assert resp.headers['x-b3-spanid'] == span_id
+    assert resp.headers['x-b3-traceid'] == trace_id
     await middleware.tracer.close()
 
 
@@ -49,12 +47,10 @@ async def test_dispatch_trace(app, dummy_request, next_response):
         str(middleware.tracer._transport._address)
         == "http://localhost:9411/api/v2/spans"
     )
-    assert dict(resp.headers) == {
-        "x-b3-flags": "0",
-        "x-b3-sampled": "1",
-        "x-b3-spanid": resp.headers["x-b3-spanid"],
-        "x-b3-traceid": resp.headers["x-b3-traceid"],
-    }
+    assert resp.headers['x-b3-flags'] == "0"
+    assert resp.headers['x-b3-sampled'] == "1"
+    assert resp.headers['x-b3-spanid'] == resp.headers["x-b3-spanid"]
+    assert resp.headers['x-b3-traceid'] == resp.headers["x-b3-traceid"]
     await middleware.tracer.close()
 
 
@@ -76,12 +72,10 @@ async def test_dispatch_trace_buggy_headers(app, dummy_request, next_response):
     )
     assert middleware.tracer is not None
     assert middleware.tracer._transport is not None
-    assert dict(resp.headers) == {
-        "x-b3-flags": "0",
-        "x-b3-sampled": "1",
-        "x-b3-spanid": resp.headers["x-b3-spanid"],
-        "x-b3-traceid": resp.headers["x-b3-traceid"],
-    }
+    assert resp.headers['x-b3-flags'] == "0"
+    assert resp.headers['x-b3-sampled'] == "1"
+    assert resp.headers['x-b3-spanid'] == resp.headers["x-b3-spanid"]
+    assert resp.headers['x-b3-traceid'] == resp.headers["x-b3-traceid"]
     # we cannot reuse the traceid if the span id was missing
     assert trace_id != resp.headers["x-b3-spanid"]
     await middleware.tracer.close()
